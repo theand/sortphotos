314dd035432aec66abb7c41a7f8e9f2cca58cb6a
Heesang Chae <theand@gmail.com>
2026-04-05 03:18:01 +0900

    오류 잡기

--- src/sortphotos.py
+++ src/sortphotos.py
@@ -64,7 +64,7 @@ def parse_date_exif(date_string, group):
     second = 0
 
     if len(elements) > 1:
-        time_entries = re.split('(\+|-|Z)', elements[1])  # ['HH:MM:SS', '+', 'HH:MM']
+        time_entries = re.split(r'(\+|-|Z)', elements[1])  # ['HH:MM:SS', '+', 'HH:MM']
         time = time_entries[0].split(':')  # ['HH', 'MM', 'SS']
 
         if len(time) == 3:
